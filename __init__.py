960632b8a9c60ea2a96c15b53502c9d344a2747e
AhBumm <shacolmx@gmail.com>
2025-10-31 11:10:25 +0800

    update

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from .unfake_node import *
 
 ## Meneger Mapping
 NODE_CLASS_MAPPINGS = {
-    "CustomUnfake": CustomUnfake,
+    "Unfake_PixelateTools": CustomUnfake,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
 }
